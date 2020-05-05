573ba668e602d86de4903a03adffac98aab0f62a
madhuvayya <madhu.vayya@gmail.com>
2020-05-05 09:03:15 +0530

    Added feature to find employee present or absent

--- EmpWageCalc.java
+++ EmpWageCalc.java
@@ -1,5 +1,13 @@
 public class EmpWageCalc {
+
 	public static void main(String []args){
 		System.out.println("Welcome to Employee Wage Computation");
+
+		int IS_PRESENT = 1;
+		double empCheck = Math.floor( Math.random() * 10 ) % 2;
+			if( empCheck == IS_PRESENT )
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is Absent");
 	}
 }
