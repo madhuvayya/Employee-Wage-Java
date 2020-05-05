b7392478173745758bffda4c1a30e4342698a744
madhuvayya <madhu.vayya@gmail.com>
2020-05-05 09:23:52 +0530

    Added part time employee feature

--- EmpWageCalc.java
+++ EmpWageCalc.java
@@ -3,15 +3,18 @@ public class EmpWageCalc {
 	public static void main(String []args){
 		System.out.println("Welcome to Employee Wage Computation");
 
-		int IS_PRESENT = 1;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		int empHrs = 0;
 		int empWage = 0 ;
 
-		double empCheck = Math.floor( Math.random() * 10 ) % 2;
-			if( empCheck == IS_PRESENT )
+		double empCheck = Math.floor( Math.random() * 10 ) % 3;
+			if( empCheck == IS_FULL_TIME )
 				empHrs = 8;
+			else if( empCheck == IS_PART_TIME )
+				empHrs = 4;
 			else
 				empHrs = 0;
 
