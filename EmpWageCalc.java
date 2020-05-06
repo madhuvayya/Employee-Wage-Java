69ff447a4f28bfd4d64aa58690f4106b6bebac40
madhuvayya <madhu.vayya@gmail.com>
2020-05-06 10:58:20 +0530

    Added feature to store employee daily wage

--- EmpWageCalc.java
+++ EmpWageCalc.java
@@ -6,10 +6,34 @@ public class EmpWageCalc {
 	private static final int IS_PART_TIME = 2;
 
 	private static int empHrs;
-	private static int numOfCompanies;
+	private static int numOfCompanies = 0;
 
-	private static ArrayList<Integer> arrayList = new ArrayList<Integer>(); 
+	private static ArrayList<CompanyEmpWage> companiesArrayList = new ArrayList<CompanyEmpWage>();
+	private static ArrayList<Integer> wagesArrayList = new ArrayList<Integer>(); 
 
+	public static void addCompany(String company, int empRatePerHr, int maxWorkingDays, int maxWorkingHrsPerMonth){
+		CompanyEmpWage cew = new CompanyEmpWage (company,empRatePerHr,maxWorkingDays, maxWorkingHrsPerMonth);
+		companiesArrayList.add(cew);
+		numOfCompanies++;
+	}	
+
+	public static void calcEmpWageForEachCompany(){
+		for(int company=0; company<numOfCompanies ; company++){
+			int totalWage = calcEmpWage(companiesArrayList.get(company));
+			System.out.print(companiesArrayList.get(company).company+" company employee Daily wages:");
+			printCompanyEmpDailyWage(companiesArrayList.get(company).numOfWorkingDays);
+			System.out.println();
+			System.out.println("totalWage"+totalWage);
+		}
+	}		
+
+	public static void printCompanyEmpDailyWage(int numOfWorkingDays){
+		for(int workingDay=0; workingDay < numOfWorkingDays; workingDay++){
+			System.out.print(wagesArrayList.get(workingDay)+" ");			
+		}
+        }
+	
+	
 	public static int empWorkingHours(){
 		int empCheck = (int) Math.floor( Math.random() * 10 ) % 3;
 
@@ -26,27 +50,26 @@ public class EmpWageCalc {
 		return empHrs;
 	}
 
-	public static void calcEmpWageForCompany(CompanyEmpWage cew){
+	public static int calcEmpWage(CompanyEmpWage cew){
 
-		int empWorkingHrs = 0, totalEmpWage = 0, empWorkingDays = 0 ;
+		int totalWorkingHrs = 0, totalEmpWage = 0, empWorkingDays = 0, empWage = 0;
 
-		while( empWorkingDays < cew.numOfWorkingDays && empWorkingHrs <= cew.maxHoursPerMonth ){
-
+		while( empWorkingDays < cew.numOfWorkingDays && totalWorkingHrs <= cew.maxHoursPerMonth ){
 			empWorkingDays++;
-			empWorkingHrs += empWorkingHours();
+			int empWorkingHrs = empWorkingHours();
+			totalWorkingHrs += empWorkingHrs; 
+			wagesArrayList.add(empWorkingHrs * cew.empRatePerHour);				
 		}
-		totalEmpWage = empWorkingHrs * cew.empRatePerHour;
-		arrayList.add(totalEmpWage);		
-		System.out.println(cew.company+" Employee Wage is :"+ totalEmpWage);
+		totalEmpWage = totalWorkingHrs * cew.empRatePerHour;		
+		return totalEmpWage; 
 	}
 
 	public static void main(String []args){
 		System.out.println("Welcome to Employee Wage Computation");
 
-		CompanyEmpWage cew1 = new CompanyEmpWage ("DMart",20,2,10);
-		calcEmpWageForCompany(cew1);
+		addCompany("DMart",20,2,10);
+		addCompany("Reliance",10,4,20);
 
-		CompanyEmpWage cew2 = new CompanyEmpWage ("Reliance",10,4,20);
-		calcEmpWageForCompany(cew2);
+		calcEmpWageForEachCompany();
 	}
 }
